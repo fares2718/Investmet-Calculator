1eaf9af5e16b3637f342ff9d57900c5371eade38
fares2.718 <faresobaid2715@gmail.com>
2026-01-05 13:18:28 +0400

    finished project essantials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { AnnualData } from './annual-data.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  annualData!: AnnualData[];
+
+  onCalculate(annualData: AnnualData[]) {
+    this.annualData = annualData;
+  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserInputService } from './user-input.service';
+import { AnnualData } from '../annual-data.model';
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<AnnualData[]>();
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestmen = '0';
+  enterdExpectedReturn = '5';
+  enterdDuration = '10';
+
+  private userInputService = inject(UserInputService);
+
+  onCalculat() {
+    this.calculate.emit(
+      this.userInputService.calculateInvestmentResults(
+        Number(this.enteredInitialInvestment),
+        Number(this.enteredAnnualInvestmen),
+        Number(this.enterdExpectedReturn),
+        Number(this.enterdDuration)
+      )
+    );
+  }
+}
